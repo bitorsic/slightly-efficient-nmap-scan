cb9cc4a7f0153b2835fa0aca2cb37a7e5ece4b71
Yash Jaiswal <67980466+bitorsic@users.noreply.github.com>
2021-08-02 20:49:06 +0530

    Create scan.py

---
+++ scan.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import os, sys
+
+if len(sys.argv) == 2:
+	ip = sys.argv[1]
+
+	nlports = os.popen('nmap -T4 -p- {} | grep "open" | cut -d "/" -f 1'.format(ip), 'r').readlines()
+
+	ports = []
+
+	for port in nlports:
+		ports.append(port.strip())
+
+	portsString = ','.join(ports)
+
+	os.system('nmap -T4 -p {} -A {}'.format(portsString, ip))
+
+else:
+	print("Enter the IP address as an argument")
+	print("Syntax: ./scan.py 127.0.0.1")
